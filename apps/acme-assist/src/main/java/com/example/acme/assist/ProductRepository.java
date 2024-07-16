dfd270875edd36b27034c2ef131aa2ce16a55cfb
Stuart Charlton <stuart.charlton@broadcom.com>
2024-07-16 11:46:54 -0600

    basic mods to get it running on TPCF

--- apps/acme-assist/src/main/java/com/example/acme/assist/ProductRepository.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/ProductRepository.java
@@ -7,12 +7,14 @@ import jakarta.annotation.PostConstruct;
 import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -20,11 +22,20 @@ public class ProductRepository {
 
     private static final Logger log = LoggerFactory.getLogger(ProductRepository.class);
     private static List<Product> products;
-    private String catalogService;
     private final RestTemplate restTemplate = new RestTemplate();
-
-    public ProductRepository(@Value("${catalogService:http://catalog-service}") String catalogService) {
-        this.catalogService = catalogService;
+    private final DiscoveryClient discoveryClient;
+    
+    public ProductRepository(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+    private String getCatalogService() {
+        List<ServiceInstance> services = discoveryClient.getInstances("acme-catalog");
+        if (services.size() > 0) 
+            return services.get(0).getUri().toString();
+        else    {
+            log.warn("Can't find acme-catalog in registry, no products returned");
+            return null;
+        }
     }
 
     public Product getProductById(String id) {
@@ -32,6 +43,10 @@ public class ProductRepository {
             return null;
         }
         try {
+            String catalogService = getCatalogService();
+            if (catalogService == null) 
+                return null;
+            
             var response = this.restTemplate.getForEntity(catalogService + "/products/" + id, CatalogProductResponse.class);
             log.info("Response code from catalog-service: {}", response.getStatusCode());
             return response.getBody().getData();
@@ -42,6 +57,9 @@ public class ProductRepository {
     }
 
     public void refreshProductList() {
+        String catalogService = getCatalogService();
+        if (catalogService == null) 
+            return;
         ResponseEntity<CatalogProductListResponse> response = this.restTemplate
                 .getForEntity(catalogService + "/products", CatalogProductListResponse.class);
         products = response.getBody().getData();
@@ -50,6 +68,10 @@ public class ProductRepository {
     @PostConstruct
     public List<Product> getProductList() {
         if (products == null) {
+            String catalogService = getCatalogService();
+            if (catalogService == null) 
+                return new ArrayList<Product>();
+
             ResponseEntity<CatalogProductListResponse> response = this.restTemplate
                     .getForEntity(catalogService + "/products", CatalogProductListResponse.class);
             products = response.getBody().getData();
--- apps/acme-assist/src/main/java/com/example/acme/assist/config/FitAssistConfiguration.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/config/FitAssistConfiguration.java
@@ -3,6 +3,9 @@ package com.example.acme.assist.config;
 import com.example.acme.assist.mongodb.CosmosDBVectorStore;
 import com.example.acme.assist.vectorstore.LazyCalculateSimpleVectorStore;
 import org.springframework.ai.embedding.EmbeddingClient;
+import org.springframework.ai.vectorstore.PgVectorStore;
+import org.springframework.ai.vectorstore.PgVectorStore.PgDistanceType;
+import org.springframework.ai.vectorstore.PgVectorStore.PgIndexType;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class FitAssistConfiguration {
@@ -19,7 +23,7 @@ public class FitAssistConfiguration {
     private Resource vectorDbResource;
 
     @Bean
-    @ConditionalOnProperty(value="vectorstore", havingValue = "simple", matchIfMissing = true)
+    @ConditionalOnProperty(value="vectorstore", havingValue = "simple", matchIfMissing = false)
     public SimpleVectorStore simpleVectorStore(EmbeddingClient embeddingClient) {
         SimpleVectorStore simpleVectorStore = new LazyCalculateSimpleVectorStore(embeddingClient);
         simpleVectorStore.load(vectorDbResource);
@@ -34,4 +38,15 @@ public class FitAssistConfiguration {
         return new CosmosDBVectorStore(vectorDbResource, mongoTemplate, embeddingClient);
     }
 
+    @Bean
+    @ConditionalOnProperty(value = "vectorstore", havingValue = "postgres")
+    public VectorStore postgresVectorStore(JdbcTemplate jcbcTemplate, EmbeddingClient embeddingClient) {
+         return new PgVectorStore( jcbcTemplate, embeddingClient,
+            4096,
+            PgVectorStore.PgDistanceType.COSINE_DISTANCE,
+            false,
+            PgVectorStore.PgIndexType.NONE);
+    }
+ 
+    
 }
