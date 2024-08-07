7813be96ef058f017ad9bd090535b29407c42098
Carlos Poon <carlos.poon@broadcom.com>
2024-08-07 12:49:08 -0400

    Introduce config and scripts for running local Spring Cloud Gateway and Spring Authorization configs and routes needed to standup Identity.
    Co-authored-by: Brian Shim <brian.shim@broadcom.com>
    Co-authored-by: Carlos Poon <carlos.poon@broadcom.com>
    Co-authored-by: David Joo <david.joo@braodcom.com>
    Co-authored-by: Dayo Ajayi <dayo.ajayi@broadcom.com>

--- apps/acme-identity/src/main/java/com/example/acme/identity/WhoAmIController.java
+++ apps/acme-identity/src/main/java/com/example/acme/identity/WhoAmIController.java
@@ -1,15 +1,14 @@
 package com.example.acme.identity;
 
+import static java.util.Collections.emptyMap;
 import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static java.util.Collections.emptyMap;
 
 @RestController
 public class WhoAmIController {
@@ -17,21 +16,18 @@ public class WhoAmIController {
 	private final Logger log = LoggerFactory.getLogger(WhoAmIController.class);
 
 	@GetMapping("/whoami")
-	public Map<String, String> getUserInfo(OAuth2AuthenticationToken principal) {
+	public Map<String, String> getUserInfo(Authentication authentication) {
 		log.debug("/whoami endpoint is triggered.");
 
-		if (principal == null) {
+		if (authentication == null) {
 			log.warn("Authentication is null cannot extract user info");
 			return emptyMap();
 		}
 
-		String attributeName = principal.getPrincipal().getAttribute("name");
-		if (attributeName == null) {
-			attributeName = "no name";
-		}
+
 		return Map.of(
-				"userId", principal.getName(),
-				"userName", attributeName
+				"userId", authentication.getName(),
+				"userName", authentication.getName()
 		);
 	}
 
