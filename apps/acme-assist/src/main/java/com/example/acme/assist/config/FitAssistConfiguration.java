3e4296522f228f2729512b7ea923c7bcc9fe111e
Stuart Charlton <stuart.charlton@broadcom.com>
2024-08-19 03:26:42 -0600

    Remove vector store jobs, vector store will init on launch

--- apps/acme-assist/src/main/java/com/example/acme/assist/config/FitAssistConfiguration.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/config/FitAssistConfiguration.java
@@ -1,15 +1,10 @@
 package com.example.acme.assist.config;
 
-import com.example.acme.assist.vectorstore.LazyCalculateSimpleVectorStore;
+import com.example.acme.assist.ProductRepository;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.DefaultChatClientBuilder;
-import org.springframework.ai.embedding.EmbeddingModel;
-import org.springframework.ai.openai.OpenAiChatModel;
-import org.springframework.ai.vectorstore.PgVectorStore;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -18,28 +13,18 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 public class FitAssistConfiguration {
 
-    @Value("classpath:/vector_store_dimension_1536.json")
-    private Resource vectorDbResource;
-
-    @Bean
-    @ConditionalOnProperty(value="vectorstore", havingValue = "simple", matchIfMissing = false)
-    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
-        SimpleVectorStore simpleVectorStore = new LazyCalculateSimpleVectorStore(embeddingModel);
-        simpleVectorStore.load(vectorDbResource);
-        return simpleVectorStore;
-    }
-
+    @Autowired
+    private VectorStore vectorStore;
+    
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
         return chatClientBuilder.build();
     }
 
-    @Bean
-    @ConditionalOnProperty(value = "vectorstore", havingValue = "postgres")
-    public VectorStore postgresVectorStore(JdbcTemplate jcbcTemplate, EmbeddingModel embeddingModel) {
-         return new PgVectorStore( jcbcTemplate, embeddingModel, 1536,
-            PgVectorStore.PgDistanceType.COSINE_DISTANCE,
-            false,
-            PgVectorStore.PgIndexType.NONE, false);
-    }
+	@Bean
+	public VectorStoreInitializer vectorStoreInitializer(ProductRepository repo) {
+		return new VectorStoreInitializer(repo, this.vectorStore);
+	}
+
 }
+
--- apps/acme-assist/src/main/java/com/example/acme/assist/config/SpringApplicationContextInitializer.java
+++ apps/acme-assist/src/main/java/com/example/acme/assist/config/SpringApplicationContextInitializer.java
@@ -1,27 +1,17 @@
 package com.example.acme.assist.config;
 
-import io.pivotal.cfenv.core.CfCredentials;
 import io.pivotal.cfenv.core.CfEnv;
 import io.pivotal.cfenv.core.CfService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.Profiles;
-import org.springframework.core.env.PropertySource;
+
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SpringApplicationContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
@@ -29,7 +19,6 @@ public class SpringApplicationContextInitializer implements ApplicationContextIn
 
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
-        ConfigurableEnvironment appEnvironment = applicationContext.getEnvironment();
         CfEnv cfEnv = new CfEnv();
 
         List<CfService> services = cfEnv.findAllServices();
@@ -40,18 +29,6 @@ public class SpringApplicationContextInitializer implements ApplicationContextIn
         logger.info("Found services " + StringUtils.collectionToCommaDelimitedString(serviceNames));
 
 
-        List<CfService> llmServices = cfEnv.findServicesByTag("llm");
-        if (!llmServices.isEmpty()) {
-            CfCredentials llmCredentials = cfEnv.findCredentialsByTag("llm");
-            
-            String openAiUrl = (String) llmCredentials.getMap().get("api_base");
-            if (openAiUrl.endsWith("/v1")) 
-                openAiUrl = openAiUrl.substring(0, openAiUrl.length() - 3);
-
-            appEnvironment.getSystemProperties().put("vectorstore", "postgres");         
-            appEnvironment.getSystemProperties().put("spring.ai.openai.base-url", openAiUrl);   
-            appEnvironment.getSystemProperties().put("spring.ai.openai.api-key", llmCredentials.getMap().get("api_key"));
-        }
 
     }
 }
