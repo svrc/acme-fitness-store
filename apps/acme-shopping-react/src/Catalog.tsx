762d9395d4ae281bf4d6aaec917f7d944422fbc4
tone2k <penguinprofession@icloud.com>
2024-08-21 14:17:43 -0400

    catalog items fix

--- apps/acme-shopping-react/src/Catalog.tsx
+++ apps/acme-shopping-react/src/Catalog.tsx
@@ -1,58 +1,35 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Grid, Paper, Stack, Typography } from "@mui/material";
-import { fetchProducts, Product } from './api/productClient';
+import React from 'react';
+import { Card, CardActionArea, Grid, Stack, Typography } from "@mui/material";
+import { useGetProducts } from './hooks/ProductHooks';
+import { Link as RouterLink } from "react-router-dom";
 
-export default function Home() {
-    const [catalog, setCatalog] = useState<Product[]>([]);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-    const navigate = useNavigate();
+export default function Catalog() {
+    const { data, error, isLoading } = useGetProducts();
 
-    useEffect(() => {
-        const loadProducts = async () => {
-            try {
-                const products = await fetchProducts();
-                setCatalog(products);
-                console.log(products);
-            } catch (error) {
-                setError('Failed to load products');
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error fetching data</div>;
 
-        loadProducts();
-    }, []);
-
-    if (loading) {
-        return <div>Loading...</div>;
+    if (!data || !data.data || !Array.isArray(data.data)) {
+        return <div>No products available</div>;
     }
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    const handleProductClick = (id: string) => {
-        navigate(`/products/${id}`);
-    };
 
     return (
         <Stack alignItems='center'>
             <h2>Products</h2>
             <h5>Best in Class Products to keep you fit</h5>
 
-            <Grid container spacing={3} sx={{width: "75%"}}>
-                {catalog.map((item, index) => (
+            <Grid container spacing={3} sx={{ width: "75%" }}>
+                {data.data.map((item, index) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                        <Paper>
+                    <Card key={index}>
+                        <CardActionArea sx={{ height: "300px", p: "20px" }} component={RouterLink} to={`/product/${item.id}`}>
                             <Stack alignItems='center' spacing={2}>
-                                <img src={item.imageUrl1} width="200px"></img>
-                                <Typography>{item.name}</Typography>
+                                <img src={item.imageUrl1} width="160px" alt={`image-${item.name}`} />
+                                <Typography align='center'>{item.name}</Typography>
                                 <Typography>{`USD ${item.price}`}</Typography>
                             </Stack>
-                        </Paper>
+                        </CardActionArea>
+                    </Card>
                     </Grid>
                 ))}
             </Grid>
